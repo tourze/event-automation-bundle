18418eb61da50e4ec02dd2970b4fefed27ed4e39
tourze-ci <you@example.com>
2025-04-12 11:01:37 +0000

    refactor(event-automation-bundle): 移除不再使用的 AdminMenu 类并简化 EventAutomationBundle

--- src/EventAutomationBundle.php
+++ src/EventAutomationBundle.php
@@ -3,16 +3,7 @@
 namespace EventAutomationBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tourze\BundleDependency\BundleDependencyInterface;
-use Tourze\EasyAdmin\Attribute\Permission\AsPermission;
 
-#[AsPermission(title: '事件自动化')]
-class EventAutomationBundle extends Bundle implements BundleDependencyInterface
+class EventAutomationBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [
-            \AmisBundle\AmisBundle::class => ['all' => true],
-        ];
-    }
 }
